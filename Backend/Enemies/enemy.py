5f603b236373c5c03e110e0537e574c86c226b63
Hammond865 <rileyhammond1@gmail.com>
2024-10-14 13:29:39 -0400

    Fixed bug that sometimes doubled enemy speed

--- Backend/Enemies/enemy.py
+++ Backend/Enemies/enemy.py
@@ -9,7 +9,7 @@ class Enemy:
         self.pos_y = checkpoints[0][1]
         self.sprite = pygame.Rect(checkpoints[0][0], checkpoints[0][1], 20, 20)
         self.color = (200, 0, 0)
-        self.curr_checkpoint = 0
+        self.curr_checkpoint = 1
         self.speed = .3
 
     def get_x(self):
@@ -24,43 +24,44 @@ class Enemy:
     def advance(self):
         check_x = self.checkpoints[self.curr_checkpoint][0]
         check_y = self.checkpoints[self.curr_checkpoint][1]
-        # margin = self.speed/10
+        margin = self.speed/100
         
         # Left of checkpoint
         if self.pos_x < check_x:
+            found_direction = True
             # Up and to left of checkpoint
             if self.pos_y < check_y:
                 self.pos_x += self.speed
                 self.pos_y += self.speed
             # Down and to left of checkpoint
-            if self.pos_y > check_y:
+            elif self.pos_y > check_y:
                 self.pos_x += self.speed
                 self.pos_y -= self.speed
             # Left and in line with checkpoint
-            if check_y - self.speed < self.pos_y < check_y + self.speed:
+            if check_y - margin < self.pos_y < check_y + margin:
                 self.pos_x += self.speed
 
         # Right of checkpoint
-        if self.pos_x > check_x:
+        elif self.pos_x > check_x:
             # Up and to right of checkpoint
             if self.pos_y < check_y:
                 self.pos_x -= self.speed
                 self.pos_y += self.speed
             # Down and to right of checkpoint
-            if self.pos_y > check_y:
+            elif self.pos_y > check_y:
                 self.pos_x -= self.speed
                 self.pos_y -= self.speed
             # Right and in line with checkpoint
-            if check_y - self.speed < self.pos_y < check_y + self.speed:
+            if check_y - margin < self.pos_y < check_y + margin:
                 self.pos_x -= self.speed
 
         # In line with checkpoint
-        if check_x - self.speed < self.pos_x < check_x + self.speed:
+        if check_x - margin < self.pos_x < check_x + margin:
             # Up and in line with checkpoint
             if self.pos_y < check_y:
                 self.pos_y += self.speed
             # Down and in line with checkpoint
-            if self.pos_y > check_y:
+            elif self.pos_y > check_y:
                 self.pos_y -= self.speed
             # Perfectly in line with checkpoint
             if check_y - self.speed < self.pos_y < check_y + self.speed:
