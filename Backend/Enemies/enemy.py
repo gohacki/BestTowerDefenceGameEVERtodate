014a13465c612f46f2c33fd6039f33ed0660d1d3
Hammond865 <rileyhammond1@gmail.com>
2024-10-14 13:33:58 -0400

    Simplified if logic

--- Backend/Enemies/enemy.py
+++ Backend/Enemies/enemy.py
@@ -28,45 +28,26 @@ class Enemy:
         
         # Left of checkpoint
         if self.pos_x < check_x:
-            found_direction = True
-            # Up and to left of checkpoint
-            if self.pos_y < check_y:
-                self.pos_x += self.speed
-                self.pos_y += self.speed
-            # Down and to left of checkpoint
-            elif self.pos_y > check_y:
-                self.pos_x += self.speed
-                self.pos_y -= self.speed
-            # Left and in line with checkpoint
-            if check_y - margin < self.pos_y < check_y + margin:
-                self.pos_x += self.speed
+            self.pos_x += self.speed
 
         # Right of checkpoint
-        elif self.pos_x > check_x:
-            # Up and to right of checkpoint
-            if self.pos_y < check_y:
-                self.pos_x -= self.speed
-                self.pos_y += self.speed
-            # Down and to right of checkpoint
-            elif self.pos_y > check_y:
-                self.pos_x -= self.speed
-                self.pos_y -= self.speed
-            # Right and in line with checkpoint
-            if check_y - margin < self.pos_y < check_y + margin:
-                self.pos_x -= self.speed
+        if self.pos_x > check_x:
+            self.pos_x -= self.speed
+
+        # Above checkpoint
+        if self.pos_y < check_y:
+            self.pos_y += self.speed
+
+        # Below checkpoint
+        if self.pos_y > check_y:
+            self.pos_y -= self.speed
 
         # In line with checkpoint
         if check_x - margin < self.pos_x < check_x + margin:
-            # Up and in line with checkpoint
-            if self.pos_y < check_y:
-                self.pos_y += self.speed
-            # Down and in line with checkpoint
-            elif self.pos_y > check_y:
-                self.pos_y -= self.speed
-            # Perfectly in line with checkpoint
             if check_y - self.speed < self.pos_y < check_y + self.speed:
                 self.curr_checkpoint += 1
                 if self.curr_checkpoint == len(self.checkpoints):
                     self.curr_checkpoint -= 1
 
+        # Modify sprite
         self.sprite = pygame.Rect(self.pos_x, self.pos_y, 20, 20)
