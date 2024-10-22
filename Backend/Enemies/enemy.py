64ffd9348dae506591f78233a8f403b6c740a08d
Eve Hammond <rileyhammond1@gmail.com>
2024-10-21 20:26:11 -0400

    Simplified the implementation of waves

--- Backend/Enemies/enemy.py
+++ Backend/Enemies/enemy.py
@@ -80,6 +80,13 @@ class Enemy:
             self.counter = 0
 
 
+def generate_wave(num_spawns, canvas, checkpoints):
+    wave = []
+    for i in range(num_spawns):
+        wave.append(Enemy(canvas, checkpoints))
+    return wave
+
+
 # Just used as a translator between GameManager and Enemy
 class EnemyManager:
     def __init__(self, canvas, checkpoints):
@@ -93,24 +100,23 @@ class EnemyManager:
         self.timer_target = 750
         # Counter to check if it's time to spawn an enemy yet; start at full
         self.timer_counter = self.timer_target
-        
-        # Populate list
-        for i in range(self.spawn_target):
-            self.enemies.append(Enemy(canvas, checkpoints))
+        self.wave = generate_wave(self.spawn_target, canvas, checkpoints)
 
     def update(self):
-        # Advance all spawned enemies
-        for i in range(self.spawn_counter):
-            self.enemies[i].advance()
-
         # If not all have been spawned
         if self.spawn_counter < self.spawn_target:
-            # If it's time to spawn a new one, reset timer and allow next spawn
+            # If it's time to spawn a new one, reset timer and do so
             if self.timer_counter == self.timer_target:
-                self.timer_counter = 0
+                # Pull a new enemy from the wave
+                self.enemies.append(self.wave[self.spawn_counter])
                 self.spawn_counter += 1
+                self.timer_counter = 0
             # Otherwise, increment timer
             self.timer_counter += 1
+
+        # Advance all spawned enemies
+        for enemy in self.enemies:
+            enemy.advance()
 
     def render(self, screen):
         # Draw all spawned enemies
